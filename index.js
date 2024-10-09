d6dfebc637861103a701ddfd3600b7f384d260f8
Avionte Williams <aviontewilliams@gmail.com>
2024-10-09 11:03:15 -0400

    completed1 and half of two

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+const multiplesOfSixAndNine = () => {
+  for (let i = 1; i < 100; i++) {
+    if (i % 6 === 0 && i % 9 === 0) {
+      console.log(i);
+    }
+  }
+};
+console.log(multiplesOfSixAndNine());
 
+// const greaterNum = (num1, num2) => {
+//   if (num1 > num2) {
+//     return num1;
+//   } else if (num1 < num2) {
+//     return num2;
+//   } else if (num1 === num2) {
+//     return "both integers are equal";
+//   } else if (num1) {
+//     return null;
+//   }
+// };
+
+greaterNum;
+console.log(greaterNum(10, 7)); //returns 10
+console.log(greaterNum(1.14, 1.14)); //returns "both integers are equal"
+console.log(greaterNum("21", 21)); //returns null
+console.log(greaterNum("21", "21")); //returns null
+
+const sumOfFourAndSix = () => {
+  for (let i = 1; i <= 1000; i++)
+    if (i % 4 === 0 && i % 6 === 0) {
+    }
+};
